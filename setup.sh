457c8098f8382859ebc67d92c1e441d52807f6ce
Man <sytect@hotmail.com>
2020-02-21 22:06:50 -0500

    more intensive deployment

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+echo "Adding path.txt files"
 cd src/
 SRC=$(pwd)
 echo "$SRC" > path.txt
 echo "$SRC" > ../path.txt
 echo "$SRC" > ../setup/path.txt
-cd ../setup/
+echo "Added path.txt files"
+cd ../
+echo "Installing latest version of pip"
+python3 -m pip install --user --upgrade pip
+echo "Installing virtualenv"
+python3 -m pip install --user virtualenv
+echo "Setting up virtual environment"
 python3 -m venv env
-
+echo "Entering virtual environment"
+source env/bin/activate
+echo "Installing dependencies in virtual environment"
+bash setup/setup.sh
